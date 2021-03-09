4aa13086c5cc992c807526e0913280b871f325cf
JunSeong <ebseud6135@naver.com>
2021-03-09 13:15:02 +0900

    style: prettier 적용

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,33 +1,22 @@
 <template>
   <v-app>
     <div>
-
-
-      <v-app-bar
-          color="black accent-4"
-          dense
-          dark
-          height="80"
-      >
+      <v-app-bar color="black accent-4" dense dark height="80">
         <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
         <v-toolbar-title>Luxury Shop</v-toolbar-title>
 
         <v-spacer></v-spacer>
-        <v-col
-            cols="6"
-            md="3"
-        >
+        <v-col cols="6" md="3">
           <v-form ref="form">
             <v-text-field
-                v-model="model"
-                :counter="max"
-                :rules="rules"
-                label="Luxury shop search"
+              v-model="model"
+              :counter="max"
+              :rules="rules"
+              label="Luxury shop search"
             ></v-text-field>
           </v-form>
         </v-col>
-
 
         <v-btn icon>
           <v-icon>mdi-magnify</v-icon>
@@ -36,62 +25,42 @@
           <v-icon>mdi-cart</v-icon>
         </v-btn>
 
-        <v-menu
-            left
-            bottom
-        >
+        <v-menu left bottom>
           <template v-slot:activator="{ on, attrs }">
-            <v-btn
-                icon
-                v-bind="attrs"
-                v-on="on"
-            >
+            <v-btn icon v-bind="attrs" v-on="on">
               <v-icon>mdi-dots-vertical</v-icon>
             </v-btn>
           </template>
 
           <v-list>
-            <v-list-item
-                v-for="n in 5"
-                :key="n"
-                @click="() => {}"
-            >
+            <v-list-item v-for="n in 5" :key="n" @click="() => {}">
               <v-list-item-title>Option {{ n }}</v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
       </v-app-bar>
-        <v-navigation-drawer
-            v-model="drawer"
-            absolute
-            temporary
-        >
-          <v-list
-              nav
-              dense
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list nav dense>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
           >
-            <v-list-item-group
-                v-model="group"
-                active-class="deep-purple--text text--accent-4"
-            >
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>mdi-home</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Home</v-list-item-title>
-              </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
 
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>mdi-account</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Account</v-list-item-title>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
-        </v-navigation-drawer>
-
-
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Account</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
     </div>
     <v-spacer></v-spacer>
     <v-main>
@@ -102,27 +71,26 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 import request from "request";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    HelloWorld
   },
   mounted() {
-    request("http://localhost:8090/api/hello", function (error, response, body) {
-      window.console.log('error', error);
-      window.console.log('statusCode', response && response.statusCode);
-      window.console.log('body', body);
-
+    request("http://localhost:8090/api/hello", function(error, response, body) {
+      window.console.log("error", error);
+      window.console.log("statusCode", response && response.statusCode);
+      window.console.log("body", body);
     });
   },
 
   data: () => ({
     drawer: false,
-    group: null,
-  }),
+    group: null
+  })
 };
 </script>
--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -1,39 +1,26 @@
 <template>
   <div>
-    <v-card
-        class="mx-auto overflow-hidden"
-        height="auto"
-    >
-    <v-container >
-
-      <div v-for="n in 2">
-
-        <v-hover v-slot="{ hover }">
-
-          <v-card
-              class="mx-auto"
-              color="white lighten-4"
-              max-width="350"
-          >
-            <v-img
-                :aspect-ratio="16/12"
+    <v-card class="mx-auto overflow-hidden" height="auto">
+      <v-container>
+        <div v-for="n in 2">
+          <v-hover v-slot="{ hover }">
+            <v-card class="mx-auto" color="white lighten-4" max-width="350">
+              <v-img
+                :aspect-ratio="16 / 12"
                 src="https://cdn.vuetifyjs.com/images/cards/kitchen.png"
-            >
-              <v-expand-transition>
-                <div
+              >
+                <v-expand-transition>
+                  <div
                     v-if="hover"
                     class="d-flex transition-fast-in-fast-out blue-grey darken-2 v-card--reveal display-3 white--text"
                     style="height: 100%;"
-                >
-                  $14.99
-                </div>
-              </v-expand-transition>
-            </v-img>
-            <v-card-text
-                class="pt-6"
-                style="position: relative;"
-            >
-              <v-btn
+                  >
+                    $14.99
+                  </div>
+                </v-expand-transition>
+              </v-img>
+              <v-card-text class="pt-6" style="position: relative;">
+                <v-btn
                   absolute
                   color="black"
                   class="white--text"
@@ -41,30 +28,26 @@
                   large
                   right
                   top
-              >
-                <v-icon>mdi-cart</v-icon>
-              </v-btn>
-              <div class="font-weight-light grey--text title mb-2">
-                For the perfect meal
-              </div>
-              <h3 class="display-1 font-weight-light orange--text mb-2">
-                QW cooking utensils
-              </h3>
-              <div class="font-weight-light title mb-2">
-                Our Vintage kitchen utensils delight any chef.<br>
-                Made of bamboo by hand
-              </div>
-            </v-card-text>
-          </v-card>
-        </v-hover>
-
-      </div>
-
-    </v-container>
+                >
+                  <v-icon>mdi-cart</v-icon>
+                </v-btn>
+                <div class="font-weight-light grey--text title mb-2">
+                  For the perfect meal
+                </div>
+                <h3 class="display-1 font-weight-light orange--text mb-2">
+                  QW cooking utensils
+                </h3>
+                <div class="font-weight-light title mb-2">
+                  Our Vintage kitchen utensils delight any chef.<br />
+                  Made of bamboo by hand
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-hover>
+        </div>
+      </v-container>
     </v-card>
-
   </div>
-
 </template>
 
 <script>
@@ -74,14 +57,14 @@ export default {
   // data: {
   //
   // }
-}
+};
 </script>
 <style>
 .v-card--reveal {
   align-items: center;
   bottom: 0;
   justify-content: center;
-  opacity: .5;
+  opacity: 0.5;
   position: absolute;
   width: 100%;
 }
