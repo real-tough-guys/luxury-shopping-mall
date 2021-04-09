98e42ac724e4320d7033e1229d93bf497449d63a
jhj960918 <jhj960918@gmail.com>
2021-04-09 20:44:15 +0900

    Exception 클래스 생성 ProductFindByIdException extends IllegalArgumentException

---
+++ src/main/java/com/skhu/luxuryshop/controllerAdvice/ProductExceptionControllerAdvice.java
@@ -0,0 +1,31 @@
+package com.skhu.luxuryshop.controllerAdvice;
+
+
+import com.skhu.luxuryshop.exception.ProductFindByIdException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ProductExceptionControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error( e.getMessage(), e );
+        return new ResponseEntity( e.getMessage(), HttpStatus.BAD_REQUEST );
+    }
+
+    @ExceptionHandler(ProductFindByIdException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> productFindByIdException(ProductFindByIdException e) {
+        log.error( e.getMessage(), e );
+        return new ResponseEntity( e.getMessage(), HttpStatus.BAD_REQUEST );
+    }
+
+}
---
+++ src/main/java/com/skhu/luxuryshop/exception/ProductFindByIdException.java
@@ -0,0 +1,10 @@
+package com.skhu.luxuryshop.exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class ProductFindByIdException extends IllegalArgumentException {
+    public ProductFindByIdException(String message) {
+        super( message );
+    }
+}
