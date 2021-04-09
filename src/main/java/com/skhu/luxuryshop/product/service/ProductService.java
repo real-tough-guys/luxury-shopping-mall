a1fd118e1782a913780ba13418af6ffc86f59380
jhj960918 <jhj960918@gmail.com>
2021-04-09 20:42:20 +0900

    FindByIException 추가 , update ,delete => service 추가

--- src/main/java/com/skhu/luxuryshop/product/service/ProductService.java
+++ src/main/java/com/skhu/luxuryshop/product/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.skhu.luxuryshop.product.service;
 
+
+import com.skhu.luxuryshop.exception.ProductFindByIdException;
 import com.skhu.luxuryshop.product.dto.ProductRequestDto;
 import com.skhu.luxuryshop.product.dto.ProductResponseDto;
 import com.skhu.luxuryshop.product.entity.ProductEntity;
@@ -7,10 +9,8 @@ import com.skhu.luxuryshop.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-
 import java.util.List;
 import java.util.stream.Collectors;
-
 
 @Service
 public class ProductService {
@@ -20,26 +20,38 @@ public class ProductService {
 
     public ProductService(ProductRepository productRepository) {//Constructor Injection
         this.productRepository = productRepository;
-
     }
 
     public ProductResponseDto findById(Long id) {
-        ProductEntity productEntity = productRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 상품이 없습니다. id=" + id));
-        return new ProductResponseDto(productEntity);
+        ProductEntity productEntity = productRepository.findById( id )
+                .orElseThrow( () -> new ProductFindByIdException( "상품 id : " + id + "이 존재 하지 않습니다." ) );
+        return ProductResponseDto.from( productEntity );
+    }
+
+    @Transactional
+    public Long update(Long id, ProductRequestDto productRequestDto) {
+        ProductEntity productEntity = productRepository.findById( id ).orElseThrow( () -> new ProductFindByIdException( "업데이트 하려는 상품 id : " + id + "이 존재 하지 않습니다." ) );
+        productEntity.update( productRequestDto );
+        return productEntity.getId();
     }
 
     public List<ProductResponseDto> findAll() {
         List<ProductEntity> productEntitys = productRepository.findAll();
         return productEntitys.stream()
-                .map(ProductResponseDto::new)
-                .collect(Collectors.toList());
+                .map( ProductResponseDto::from )
+                .collect( Collectors.toList() );
     }
 
     @Transactional
-    public long save(ProductRequestDto productDto) {
+    public Long save(ProductRequestDto productDto) {
         ProductEntity productEntity = productDto.toProductEntity();
-        return productRepository.save(productEntity).getId();
+        return productRepository.save( productEntity ).getId();
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        productRepository.findById( id ).orElseThrow( () -> new ProductFindByIdException( "삭제 하려는 상품 id : " + id + "이 존재 하지 않습니다." ) );
+        productRepository.deleteById( id );
     }
 
 
