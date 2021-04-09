9892f3ee8eeedab2d951a55c32a756cc92554ea7
jhj960918 <jhj960918@gmail.com>
2021-04-09 20:37:58 +0900

    @valid 검증 , delete ,update API 추기

--- src/main/java/com/skhu/luxuryshop/product/controller/ProductController.java
+++ src/main/java/com/skhu/luxuryshop/product/controller/ProductController.java
@@ -1,52 +1,51 @@
 package com.skhu.luxuryshop.product.controller;
 
 import com.skhu.luxuryshop.product.dto.ProductRequestDto;
-import com.skhu.luxuryshop.product.dto.ProductResponseDto;
 import com.skhu.luxuryshop.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/products")
+@Slf4j
 public class ProductController {
 
 
     private final ProductService productService;
 
-    public ProductController(final ProductService productService) {
-        this.productService = productService;
-    }
-
     @GetMapping
     public ResponseEntity getProducts() {
-        return ResponseEntity.ok(productService.findAll());
+        return ResponseEntity.ok( productService.findAll() );
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getUserByUserId(@PathVariable Long id) {
-        return ResponseEntity.ok(productService.findById(id));
+    public ResponseEntity getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok( productService.findById( id ) );
     }
 
     @PostMapping
-    public long registerUser(@RequestBody @Valid ProductRequestDto productDto) {
-        return productService.save(productDto);
+    public ResponseEntity<Long> registerProduct(@RequestBody @Valid ProductRequestDto productDto) {
+        productService.save( productDto );
+        return new ResponseEntity( "상품등록 성공", HttpStatus.OK );
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(
-            MethodArgumentNotValidException exception) {
-        Map<String, String> errors = new HashMap<>();
-        exception.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return ResponseEntity.badRequest().body(errors);
+    @PutMapping("/{id}")
+    public ResponseEntity<Long> updateProduct(@PathVariable Long id, @RequestBody @Valid ProductRequestDto productRequestDto) {
+        productService.update( id, productRequestDto );
+        return new ResponseEntity( "상품업데이트 성공", HttpStatus.OK );
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> deleteProduct(@PathVariable Long id) {
+        productService.delete( id );
+        return new ResponseEntity( "상품삭제 성공", HttpStatus.OK );
+    }
+
 }
