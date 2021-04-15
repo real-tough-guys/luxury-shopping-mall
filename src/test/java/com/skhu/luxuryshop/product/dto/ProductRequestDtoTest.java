feb0713808e59aeb1d5d7f382d2024a8bf19a2dc
jhj960918 <jhj960918@gmail.com>
2021-04-15 19:28:13 +0900

    ReponseDto Test = > from method entity 변환

---
+++ src/test/java/com/skhu/luxuryshop/product/dto/ProductRequestDtoTest.java
@@ -0,0 +1,34 @@
+package com.skhu.luxuryshop.product.dto;
+
+import com.skhu.luxuryshop.product.entity.ProductEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductRequestDtoTest {
+    private ProductEntity product;
+    private ProductRequestDto validProduct;
+    private ProductRequestDto noValidProduct;
+
+    @BeforeEach
+    void setUp() {
+        validProduct = new ProductRequestDto("requestDTO 테스트", "DTO 테스트", 900, "DTO 테스트", "DTO 테스트");
+    }
+
+    @DisplayName("유효한 Product 요청")
+    @Test
+    void test_toProductEntity_validProduct() {
+        product = validProduct.toProductEntity();
+        assertThat(validProduct.getName()).isEqualTo(product.getName());
+        assertThat(validProduct.getContent()).isEqualTo(product.getContent());
+        assertThat(validProduct.getPrice()).isEqualTo(product.getPrice());
+    }
+
+    @DisplayName("유효하지 않은 Product 요청")
+    @Test
+    void test_NoValid() {
+        noValidProduct = new ProductRequestDto("requestDTO 테스트", "DTO 테스트", -900, "DTO 테스트", "DTO 테스트");
+    }
+}
