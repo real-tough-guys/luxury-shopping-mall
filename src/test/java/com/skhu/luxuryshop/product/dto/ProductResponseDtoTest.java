1a3457cd9c0f4e8f7a417245ab40289c1f85f403
jhj960918 <jhj960918@gmail.com>
2021-04-15 19:28:25 +0900

    no message

---
+++ src/test/java/com/skhu/luxuryshop/product/dto/ProductResponseDtoTest.java
@@ -0,0 +1,27 @@
+package com.skhu.luxuryshop.product.dto;
+
+import com.skhu.luxuryshop.product.entity.ProductEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductResponseDtoTest {
+    private ProductEntity product;
+    private ProductResponseDto productResponseDto;
+
+    @BeforeEach
+    void setUp() {
+        product = new ProductEntity(1L, "DTO 테스트", "DTO 테스트", 900, "DTO 테스트", "DTO 테스트");
+    }
+
+    @DisplayName("form_productentity")
+    @Test
+    void test_from() {
+        productResponseDto = ProductResponseDto.from(product);
+        assertThat(product.getId()).isEqualTo(productResponseDto.getId());
+        assertThat(product.getContent()).isEqualTo(productResponseDto.getContent());
+        assertThat(product.getName()).isEqualTo(productResponseDto.getName());
+    }
+}
