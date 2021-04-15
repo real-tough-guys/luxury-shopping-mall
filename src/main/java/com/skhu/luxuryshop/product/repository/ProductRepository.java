8c5bbfbf94c1b52561bbd116363eaafebb894bfa
jhj960918 <jhj960918@gmail.com>
2021-04-15 19:21:30 +0900

    existById method 생성

--- src/main/java/com/skhu/luxuryshop/product/repository/ProductRepository.java
+++ src/main/java/com/skhu/luxuryshop/product/repository/ProductRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-
+    boolean existsById(Long id);
 }
