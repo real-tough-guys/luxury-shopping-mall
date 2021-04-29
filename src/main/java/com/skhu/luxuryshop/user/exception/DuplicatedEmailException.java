ca515ddc20ba6ed6a93edcf9a7e688aaab8627d2
sinhyuk <rlatls1997@gmail.com>
2021-04-30 00:05:07 +0900

    refactor: 사소한 코드 정렬

--- src/main/java/com/skhu/luxuryshop/user/exception/DuplicatedEmailException.java
+++ src/main/java/com/skhu/luxuryshop/user/exception/DuplicatedEmailException.java
@@ -1,7 +1,7 @@
 package com.skhu.luxuryshop.user.exception;
 
 public class DuplicatedEmailException extends RuntimeException {
-    public DuplicatedEmailException(){
+    public DuplicatedEmailException() {
         super("중복된 이메일입니다.");
     }
 }
--- src/main/java/com/skhu/luxuryshop/user/exception/NoUserFoundException.java
+++ src/main/java/com/skhu/luxuryshop/user/exception/NoUserFoundException.java
@@ -1,6 +1,6 @@
 package com.skhu.luxuryshop.user.exception;
 
-public class NoUserFoundException extends IllegalArgumentException{
+public class NoUserFoundException extends IllegalArgumentException {
     public NoUserFoundException() {
         super("해당 유저가 존재하지 않습니다.");
     }
