5455181b7b3f3229b3dc990884e8b5fe5128bf9f
jhj960918 <jhj960918@gmail.com>
2021-04-30 20:49:21 +0900

    h2 test db 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,20 @@
+INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test', ' test', 9330,'test');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (1,'test1.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (1,'test2.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (1,'test3.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (1,'test4.jpg');
 
-DROP TABLE IF EXISTS product;
+INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test2', ' test2', 500,'test2');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (2,'test2.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (2,'test3.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (2,'test4.jpg');
 
-CREATE TABLE product_entity (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  product_name VARCHAR(250) ,
-  product_content VARCHAR(250) ,
-  product_price VARCHAR(250) ,
-  product_category VARCHAR(250)
-);
+INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test3', ' test3', 500,'test3');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (3,'test3.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (3,'test1.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (3,'test2.jpg');
+
+INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test4', ' test4', 500,'test4');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (4,'test4.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (4,'test4.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (4,'test4.jpg');
