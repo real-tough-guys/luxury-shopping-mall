3effb2769cf2a41f10b359af48a70a747b710282
jhj960918 <jhj960918@gmail.com>
2021-04-30 20:50:42 +0900

    리뷰 반영

--- src/main/java/com/skhu/luxuryshop/product/service/ProductService.java
+++ src/main/java/com/skhu/luxuryshop/product/service/ProductService.java
@@ -6,13 +6,19 @@ import com.skhu.luxuryshop.product.dto.ProductRequestDto;
 import com.skhu.luxuryshop.product.dto.ProductResponseDto;
 import com.skhu.luxuryshop.product.entity.ProductEntity;
 import com.skhu.luxuryshop.product.repository.ProductRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
 
@@ -30,8 +36,7 @@ public class ProductService {
     public Long update(Long id, ProductRequestDto requestDto) {
         ProductEntity productEntity = productRepository.findById(id)
                 .orElseThrow(() -> new ProductFindByIdException("상품 id : " + id + "이 존재 하지 않습니다."));
-        productEntity.update(requestDto.getProductName(), requestDto.getProductContent(), requestDto.getProductPrice(),
-                requestDto.getProductCategory(), requestDto.getProductImageurl());
+        productEntity.update(requestDto.toProductEntity());
         return id;
     }
 
@@ -46,6 +51,20 @@ public class ProductService {
     public ProductResponseDto save(ProductRequestDto productDto) {
         ProductEntity product = productDto.toProductEntity();
         return ProductResponseDto.from(productRepository.save(product));
+    }
+
+    public void fileUpload(List<MultipartFile> files) {
+        String baseDir = System.getProperty("user.dir") + "\\src\\frontend\\src\\assets\\images\\";
+        if (files != null) {
+            try {
+                for (MultipartFile file : files) {
+                    log.info(String.valueOf(UUID.randomUUID()));
+                    file.transferTo(new File(baseDir + file.getOriginalFilename()));
+                }
+            } catch (IllegalStateException | IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Transactional
