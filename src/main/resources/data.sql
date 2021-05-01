9a36f4ae2ba17890699a9a6a0aac866c79bbfd1b
jhj960918 <jhj960918@gmail.com>
2021-05-01 23:47:48 +0900

    h2 init data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,29 @@
 INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test', ' test', 9330,'test');
 INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (1,'test1.jpg');
-INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (1,'test2.jpg');
-INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (1,'test3.jpg');
-INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (1,'test4.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (1,'test1.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (1,'test1.jpg');
 
-INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test2', ' test2', 500,'test2');
+INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test2', ' test2', 8000,'test2');
 INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (2,'test2.jpg');
-INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (2,'test3.jpg');
-INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (2,'test4.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (2,'test2.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (2,'test2.jpg');
 
-INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test3', ' test3', 500,'test3');
+INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test3', ' test3', 90000,'test3');
 INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (3,'test3.jpg');
-INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (3,'test1.jpg');
-INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (3,'test2.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (3,'test3.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (3,'test3.jpg');
 
-INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test4', ' test4', 500,'test4');
+INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('test4', ' test4', 36500,'test4');
 INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (4,'test4.jpg');
 INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (4,'test4.jpg');
 INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (4,'test4.jpg');
+
+INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('텐셀 린넨 셔츠', ' 텐셀 린넨 셔츠', 18000,'shrits');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (5,'test5-1.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (5,'test5-2.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (5,'test5-3.jpg');
+
+INSERT INTO product (product_name, product_content,product_price,product_category ) VALUES ('B-스테디 골지 니트 ', 'B-스테디 골지 니트 ', 33000,'Knit');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (6,'test6-1.jpg');
+INSERT INTO product_imageurl (product_id, product_imageurl) VALUES (6,'test6-2.jpg');
+
