8ba58dba8a7a426b88256c7172357c70f59caab6
jhj960918 <jhj960918@gmail.com>
2021-05-01 23:43:56 +0900

    no message

--- src/frontend/src/views/admin/product/AdminProductUpdate.vue
+++ src/frontend/src/views/admin/product/AdminProductUpdate.vue
@@ -2,83 +2,38 @@
   <div>
     <v-card>
       <v-toolbar color="primary" dark flat prominent>
-        <h1>Admin Detail</h1>
+        <h1>Admin Update</h1>
       </v-toolbar>
 
       <v-card max-width="450" max-height="auto" class="mx-auto my-12">
         <v-form ref="form" v-model="valid" lazy-validation>
           <v-card-title>
-            <v-text-field
-              color="primary"
-              :rules="titleRules"
-              required
-              v-model="title"
-              counter="15"
-              label="name"
-              clearable
-              clear-icon="mdi-close-circle"
-              outlined
+            <v-text-field color="primary" :rules="titleRules" required v-model="productName" counter="15" label="name"
+                          clearable clear-icon="mdi-close-circle" outlined
             ></v-text-field>
           </v-card-title>
 
           <v-card-title>
-            <v-textarea
-              color="primary"
-              :rules="contentRules"
-              v-model="content"
-              counter="50"
-              label="Content"
-              clearable
-              clear-icon="mdi-close-circle"
-              outlined
+            <v-textarea color="primary" :rules="contentRules" v-model="productContent" counter="50" label="Content"
+                        clearable clear-icon="mdi-close-circle" outlined
             ></v-textarea>
           </v-card-title>
 
           <v-card-title>
-            <v-text-field
-              color="primary"
-              v-model="price"
-              label="Price"
-              clearable
-              clear-icon="mdi-close-circle"
-              outlined
-            ></v-text-field
-            >원
+            <v-text-field color="primary" v-model="productPrice" label="Price" clearable clear-icon="mdi-close-circle"
+                          outlined></v-text-field>
+            원
           </v-card-title>
 
           <v-card-title>
-            <v-select
-              color="primary"
-              :items="items"
-              :rules="[v => !!v || 'category를 선택해주세요']"
-              required
-              v-model="category"
-              :menu-props="{ top: true, offsetY: true }"
-              label="Category"
-              outlined
+            <v-select color="primary" :items="items" :rules="[v => !!v || 'category를 선택해주세요']" required
+                      v-model="productCategory" :menu-props="{ top: true, offsetY: true }" label="Category" outlined
             ></v-select>
           </v-card-title>
 
-          <v-card-subtitle>
-            <h4>Image는 3장만 올려주세요</h4>
-          </v-card-subtitle>
-          <input
-            type="file"
-            ref="imageInput"
-            name="images[]"
-            id="photo"
-            @change="imagesAdd"
-            hidden
-            multiple
-          />
-
-          <v-btn
-            color="blue-grey"
-            class="ma-2 white--text"
-            @click="onClickImageUpload"
-          >
+          <input type="file" ref="imageInput" name="images[]" id="photo" @change="imagesAdd" hidden multiple/>
+          <v-btn color="blue-grey" class="ma-2 white--text" @click="onClickImageUpload">
             Imagae Upload
-
             <v-icon right dark>
               mdi-cloud-upload
             </v-icon>
@@ -86,54 +41,38 @@
           <v-simple-table>
             <template v-slot:default>
               <thead>
-                <tr>
-                  <th class="text-left">
-                    Image
-                  </th>
-                  <th class="text-left">
-                    Delete
-                  </th>
-                </tr>
+              <tr>
+                <th class="text-left">
+                  Image
+                </th>
+                <th class="text-left">
+                  Delete
+                </th>
+              </tr>
               </thead>
               <tbody>
-                <tr v-for="(img, i) in image" v-bind:key="i">
-                  <th>
-                    <v-img :src="img" width="130" height="130"></v-img>
-                  </th>
-                  <th>
-                    <v-btn
-                      x-small
-                      dark
-                      color="pink"
-                      v-show="image"
-                      @click="removeImage(i)"
-                    >
-                      <v-icon dark>
-                        mdi-delete
-                      </v-icon>
-                      Image {{ i + 1 }}
-                    </v-btn>
-                  </th>
-                </tr>
+              <tr v-for="(img, i) in image" v-bind:key="i">
+                <th>
+                  <v-img :src="img" width="130" height="130"></v-img>
+                </th>
+                <th>
+                  <v-btn x-small dark color="pink" v-show="image" @click="removeImage(i)">
+                    <v-icon dark>
+                      mdi-delete
+                    </v-icon>
+                    Image {{ i + 1 }}
+                  </v-btn>
+                </th>
+              </tr>
               </tbody>
             </template>
           </v-simple-table>
           <v-card-subtitle>
-            <v-checkbox
-              v-model="checkbox"
-              :rules="[v => !!v || '상품 거래에 동의 해주세요']"
-              label="Do you agree?"
-              required
+            <v-checkbox v-model="checkbox" :rules="[v => !!v || '상품 거래에 동의 해주세요']" label="Do you agree?" required
             ></v-checkbox>
           </v-card-subtitle>
         </v-form>
-        <v-btn
-          color="blue-grey"
-          block
-          class=" white--text"
-          :disabled="!valid"
-          @click="validate"
-        >
+        <v-btn color="blue-grey" block class=" white--text" :disabled="!valid" @click="validate">
           Submit
           <v-icon right color="white">
             mdi-checkbox-marked-circle
@@ -145,7 +84,9 @@
 </template>
 <script>
 import axios from "axios";
+
 export default {
+  props: ["id"],
   data() {
     return {
       valid: true,
@@ -162,34 +103,48 @@ export default {
       images: {},
       image: [],
       photoFile: [],
-      title: "미니멀자켓",
-      price: "90000",
-      category: "Outer",
-      content: "이 상품 진짜 강추!!"
+      productName: null,
+      productContent: null,
+      productPrice: null,
+      productCategory: null,
     };
   },
+  mounted() {
+    this.init();
+  },
   methods: {
+    init() {
+      this.getProduct(this.id)
+    },
+    getProduct(id) {
+      return axios
+          .get("/api/products/detail/" + id)
+          .then(res => {
+            console.log("res"+res)
+            this.productName = res.data.productName
+            this.productContent = res.data.productContent
+            this.productPrice = res.data.productPrice
+            this.productCategory = res.data.productCategory
+          })
+          .catch(err => {
+            console.log(err);
+          });
+    },
     validate() {
       this.$refs.form.validate();
       this.allSubmit();
     },
-
     onClickImageUpload() {
       this.$refs.imageInput.click();
     },
     imagesAdd(e) {
       var files = e.target.files || e.dataTransfer.files;
-
       this.images = [];
       this.image = [];
-      Array.prototype.push.apply(this.images, files); //array element add
+      Array.prototype.push.apply(this.images, files);
       if (!this.images.length) return;
-      if (this.images.length > 3) {
-        return alert("이미지는 3장만 가능해용");
-      }
       this.createImage(this.images);
     },
-
     createImage(file) {
       for (var i = 0; i < file.length; i++) {
         var reader = new FileReader();
@@ -208,28 +163,24 @@ export default {
     },
     allSubmit() {
       var frm = new FormData();
-      // this.photoFile = this.images;
       for (var i = 0; i < this.images.length; i++) {
         frm.append("files", this.images[i]);
+        frm.append("productImageurl", this.images[i].name);
       }
-
-      frm.append("title", this.title);
-      frm.append("content", this.content);
-      frm.append("price", this.price);
-      frm.append("category", this.category);
-
-      axios
-        .post("http://localhost:8090/api/image", frm, {
-          headers: {
-            "Content-Type": "multipart/form-data"
-          }
-        })
-        .then(res => {
-          console.log(res);
-        })
-        .catch(err => {
-          console.log(err);
-        });
+      frm.append("productName", this.productName);
+      frm.append("productContent", this.productContent);
+      frm.append("productPrice", this.productPrice);
+      frm.append("productCategory", this.productCategory);
+      return axios.put("/api/products/edit/" + this.id, frm, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      }).then(res => {
+        console.log(res.data);
+         this.$router.push({name: "AdminProductDetail", params: {id: res.data}});
+      }).catch(err => {
+        console.log(err);
+      });
     }
   }
 };
