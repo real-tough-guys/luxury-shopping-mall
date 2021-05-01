66575a00d6852ef802c6fb1452ca7b24ca6d4314
jhj960918 <jhj960918@gmail.com>
2021-05-01 23:43:45 +0900

    no message

--- src/frontend/src/views/admin/product/AdminProductDetail.vue
+++ src/frontend/src/views/admin/product/AdminProductDetail.vue
@@ -1,135 +1,102 @@
 <template>
   <div>
+    <loding v-if="isLoading"/>
     <v-card>
       <v-toolbar color="primary" dark flat prominent>
         <h1>Admin Detail</h1>
       </v-toolbar>
-
-
       <v-container class="grey lighten-5">
         <v-row>
           <v-col cols="12" sm="6">
             <v-card class="pa-2" outlined tile>
               <v-carousel progress-color="orange">
-                <v-carousel-item
-                    v-for="(item, i) in items"
-                    :key="i"
-                    :src="item.src"
-                    reverse-transition="fade-transition"
-                    transition="fade-transition"
-                ></v-carousel-item>
+                <v-carousel-item v-for="(item, i) in productDetail.productImageurl" :key="i"
+                                 v-bind:src="item |loadImgOrPlaceholder" width="344" height="auto"
+                                 reverse-transition="fade-transition" transition="fade-transition"></v-carousel-item>
               </v-carousel>
             </v-card>
           </v-col>
-
           <v-col cols="12" sm="6">
             <v-card class="pa-2" outlined tile>
               <v-card-title
-              ><h4>{{ detailName }}</h4>
-                <v-btn class="mx-2" fab dark small color="pink">
-                  <v-icon dark>
-                    mdi-heart
-                  </v-icon>
-                </v-btn>
+              ><h4>{{ productDetail.productName }}</h4>
               </v-card-title>
-              <v-card-text>{{ `가격 : ${price} 원 ` }}</v-card-text>
-
-              <v-btn color="blue-grey" class="ma-2 white--text">
-                Add To Cart
+              <v-card-text>{{
+                  `가격 : ${productDetail.productPrice} 원 `
+                }}
+              </v-card-text>
+              <v-btn color="blue-grey" class="ma-2 white--text" @click="updatePush">
+                UPDate
                 <v-icon right dark>
-                  mdi-cart
+                  mdi-pencil
                 </v-icon>
               </v-btn>
-
-              <v-btn color="blue-grey" class="ma-2 white--text">
-                Buy It Now
+              <v-btn color="blue-grey" class="ma-2 white--text" @click="deleteProduct">
+                DELETE
                 <v-icon right dark>
-                  mdi-arrow-right-bold
+                  mdi-delete
                 </v-icon>
               </v-btn>
-              <v-card-actions>
-                color : <v-select :items="items2" label="Standard"> </v-select
-              ></v-card-actions>
-              <v-card-actions>
-                size : <v-select :items="items3" label="Standard"> </v-select
-              ></v-card-actions>
-              <v-card-title>REALATED ITEM</v-card-title>
-              <v-carousel
-                  cycle
-                  height="150"
-                  hide-delimiter-background
-                  show-arrows-on-hover
-              >
-                <template v-slot:prev="{ on, attrs }">
-                  <v-btn color="blue-grey" v-bind="attrs" v-on="on"
-                  >Previous slide
-                  </v-btn>
-                </template>
-                <template v-slot:next="{ on, attrs }">
-                  <v-btn color="blue-grey" v-bind="attrs" v-on="on"
-                  >Next slide</v-btn
-                  >
-                </template>
-                <v-carousel-item v-for="(slide, i) in slides" :key="i">
-                  <v-sheet :color="colors[i]" height="100%">
-                    <v-row class="fill-height" align="center" justify="center">
-                      <div class="display-3">{{ slide }} Slide</div>
-                    </v-row>
-                  </v-sheet>
-                </v-carousel-item>
-              </v-carousel>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
-
-
     </v-card>
   </div>
 </template>
 <script>
 import axios from "axios";
+import Loding from "@/components/Loding.vue";
+
 export default {
+  props: ["id"],
+  components: {
+    Loding,
+  },
   data() {
     return {
-      items: [
-        {
-          src:
-              "https://image.musinsa.com/images/prd_img/2021021921333500000079112.jpg"
-        },
-        {
-          src:
-              "https://image.musinsa.com/images/prd_img/2021021921333500000011944.jpg"
-        },
-        {
-          src:
-              "https://image.musinsa.com/images/prd_img/2021021921333600000035549.jpg"
-        },
-        {
-          src:
-              "https://image.musinsa.com/images/prd_img/2021021921333600000098070.jpg"
-        }
-      ],
-      products: [
-        {
-          id: 0,
-          title: "미니멀 자켓",
-          price: 70900
-        }
-      ],
-      detailName: "미니멀 자켓",
-      price: 70900,
-      colors: [
-        "pink darken-2",
-        "pink darken-2",
-        "pink darken-2",
-        "pink darken-2",
-        "pink darken-2"
-      ],
-      items2: ["black", "white"],
-      items3: ["free"],
-      slides: ["First", "Second", "Third", "Fourth", "Fifth"]
+      isLoading: true,
+      productDetail: [],
     };
+  },
+  filters: {
+    loadImgOrPlaceholder: function (path) {
+      return require("@/assets/images/" + path)
+    }
+  },
+  computed: {},
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.getProduct(this.id)
+    },
+    getProduct(id) {
+      return axios.get("/api/products/detail/" + id)
+          .then(res => {
+            this.productDetail = res.data
+            this.isLoading = false;
+          })
+          .catch(err => {
+            console.log(err);
+          });
+    },
+    updatePush() {
+      this.$router.push({name: "AdminProductUpdate", params: {id: this.id}});
+    },
+    deleteProduct() {
+      if (confirm("삭제 하시겠습니까?")) {
+        return axios.delete("/api/products/" + this.id)
+            .then(res => {
+              this.$router.push({name: "AdminPage"});
+            })
+            .catch(err => {
+              console.log(err);
+            });
+      }
+    }
+
   }
 };
 </script>
