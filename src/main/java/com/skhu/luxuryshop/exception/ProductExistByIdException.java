e63bbf8b8fca01d262295825f0494298d970662e
jhj960918 <jhj960918@gmail.com>
2021-05-04 21:17:07 +0900

    no message

--- src/main/java/com/skhu/luxuryshop/exception/ProductExistByIdException.java
+++
@@ -1,7 +0,0 @@
-package com.skhu.luxuryshop.exception;
-
-public class ProductExistByIdException extends IllegalArgumentException {
-    public ProductExistByIdException(String message) {
-        super(message);
-    }
-}
