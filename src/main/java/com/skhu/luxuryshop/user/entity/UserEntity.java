175d1ba315fc9dbc3988f309b2bf4d3a61a8716b
sinhyuk <rlatls1997@gmail.com>
2021-05-07 04:40:41 +0900

    feat: 권한 테이블, 유저 권한 테이블 생성

--- src/main/java/com/skhu/luxuryshop/user/entity/UserEntity.java
+++ src/main/java/com/skhu/luxuryshop/user/entity/UserEntity.java
@@ -6,6 +6,7 @@ import org.hibernate.validator.constraints.Length;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.Set;
 
 @Getter
 @Builder
@@ -15,6 +16,7 @@ import javax.validation.constraints.NotBlank;
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @NotBlank(message = "이메일을 입력하세요.")
@@ -22,10 +24,17 @@ public class UserEntity {
     private String email;
 
     @NotBlank(message = "비밀번호를 입력하세요.")
-    @Length(min = 8, max = 20, message = "8~20자리의 비밀번호를 입력하세요")
     private String password;
 
     @NotBlank(message = "닉네임을 입력하세요.")
     @Length(min = 2, max = 8, message = "2~8자리의 닉네임을 입력하세요.")
     private String nickname;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")}
+    )
+    private Set<Authority> authorities;
 }
