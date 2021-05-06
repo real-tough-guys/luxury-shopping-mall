09b7a8f9f5b0e3520bc9f85af2dd88e8cda3c077
sinhyuk <rlatls1997@gmail.com>
2021-05-07 04:47:13 +0900

    feat: passwordEncoder추가

--- src/main/java/com/skhu/luxuryshop/user/service/UserSignupService.java
+++ src/main/java/com/skhu/luxuryshop/user/service/UserSignupService.java
@@ -1,23 +1,39 @@
 package com.skhu.luxuryshop.user.service;
 
 import com.skhu.luxuryshop.user.dto.UserResponseDto;
+import com.skhu.luxuryshop.user.entity.Authority;
 import com.skhu.luxuryshop.user.entity.UserEntity;
 import com.skhu.luxuryshop.user.dto.UserSignupDto;
 import com.skhu.luxuryshop.user.exception.DuplicatedEmailException;
 import com.skhu.luxuryshop.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 public class UserSignupService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public UserResponseDto save(UserSignupDto userSignupDto) {
         validateDuplicatedEmail(userSignupDto.getEmail());
-        UserEntity user = userSignupDto.toUserEntity();
+        userSignupDto.validateSamePassword(userSignupDto.getPassword(), userSignupDto.getPasswordCheck());
+
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        UserEntity user = UserEntity.builder()
+                .email(userSignupDto.getEmail())
+                .password(passwordEncoder.encode(userSignupDto.getPassword()))
+                .nickname(userSignupDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .build();
         return UserResponseDto.from(userRepository.save(user));
     }
 
