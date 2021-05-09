29ae13492e95d21e9e598e6e3194ab3c8ca7d278
sinhyuk <rlatls1997@gmail.com>
2021-05-09 11:24:35 +0900

    test: jwt추가에 따른 test코드 추가 및 수정

--- src/test/java/com/skhu/luxuryshop/user/controller/UserControllerTest.java
+++ src/test/java/com/skhu/luxuryshop/user/controller/UserControllerTest.java
@@ -2,6 +2,7 @@ package com.skhu.luxuryshop.user.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.skhu.luxuryshop.user.dto.UserLoginDto;
 import com.skhu.luxuryshop.user.dto.UserResponseDto;
 import com.skhu.luxuryshop.user.dto.UserSignupDto;
 import com.skhu.luxuryshop.user.dto.UserUpdateDto;
@@ -22,7 +23,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebM
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -37,19 +44,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(UserController.class)
 @AutoConfigureWebMvc
+@WithMockUser(username = "test1@gmail.com", roles = {"USER", "ADMIN"})
 public class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private AuthenticationManagerBuilder authenticationManagerBuilder;
     @MockBean
     private UserSignupService userSignupService;
     @MockBean
     private UserManagementService userManagementService;
     @MockBean
     private TokenProvider tokenProvider;
-    @MockBean
-    private AuthenticationManagerBuilder authenticationManagerBuilder;
 
     private UserEntity existentUser;
     private UserSignupDto normalUserSignup;
@@ -94,7 +102,7 @@ public class UserControllerTest {
         assertThat(body).isEqualTo("중복된 이메일입니다.");
     }
 
-    @DisplayName("save_정상 유저인 경우 isCreated")
+    @DisplayName("signUp_정상 유저인 경우 isCreated")
     @Test
     void test_signUp_normalUser() throws Exception {
         UserResponseDto normalUserResponse = new UserResponseDto(1L, normalUserSignup.getEmail(), normalUserSignup.getNickname(), Collections.singleton(authority));
@@ -112,7 +120,7 @@ public class UserControllerTest {
         assertThat(location).isEqualTo("/" + normalUserResponse.getId());
     }
 
-    @DisplayName("save_중복된 이메일을 가진 유저인 경우 BadRequest")
+    @DisplayName("signUp_중복된 이메일을 가진 유저인 경우 BadRequest")
     @Test
     void test_signUp_duplicatedEmailUser() throws Exception {
         when(userSignupService.save(ArgumentMatchers.any(UserSignupDto.class)))
@@ -129,7 +137,7 @@ public class UserControllerTest {
         assertThat(body).isEqualTo("중복된 이메일입니다.");
     }
 
-    @DisplayName("save_비밀번호 불일치 유저인 경우 BadRequest")
+    @DisplayName("signUp_비밀번호 불일치 유저인 경우 BadRequest")
     @Test
     void test_signUp_unmatchedPasswordUser() throws Exception {
         unmatchedPwdUserSignup = new UserSignupDto("test123@gmail.com", "password", "wrongpwd", "홍길동");
@@ -148,7 +156,7 @@ public class UserControllerTest {
         assertThat(body).isEqualTo("비밀번호가 일치하지 않습니다.");
     }
 
-    @DisplayName("details_존재하는 유저의 id인 경우")
+    @DisplayName("details(ROLE_ADMIN)_존재하는 유저의 id인 경우")
     @Test
     void test_details_existentUserId() throws Exception {
         doReturn(UserResponseDto.from(existentUser))
@@ -167,7 +175,7 @@ public class UserControllerTest {
         assertThat(userResponseDto.getNickname()).isEqualTo(existentUser.getNickname());
     }
 
-    @DisplayName("details_존재하지 않는 유저의 id인 경우 throw NoUserFoundException")
+    @DisplayName("details(ROLE_ADMIN)_존재하지 않는 유저의 id인 경우 throw NoUserFoundException")
     @Test
     void test_details_nonExistentUserId() throws Exception {
         doThrow(new NoUserFoundException())
@@ -183,7 +191,39 @@ public class UserControllerTest {
         assertThat(body).isEqualTo("해당 유저가 존재하지 않습니다.");
     }
 
-    @DisplayName("delete_존재하는 유저의 id인 경우")
+    @DisplayName("details(ROLE_USER)_로그인 되어있을 경우")
+    @Test
+    void test_myDetails_loginUser() throws Exception {
+        doReturn(UserResponseDto.from(existentUser))
+                .when(userManagementService)
+                .findByLoginUser();
+
+        MvcResult mvcResult = mockMvc.perform(get("/api/users/details"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        String body = mvcResult.getResponse().getContentAsString();
+        UserResponseDto userResponseDto = objectMapper.readValue(body, UserResponseDto.class);
+
+        assertThat(userResponseDto.getEmail()).isEqualTo(existentUser.getEmail());
+        assertThat(userResponseDto.getNickname()).isEqualTo(existentUser.getNickname());
+    }
+
+    @DisplayName("details(ROLE_USER)_로그아웃 되어있을 경우 throw AccessDeniedException")
+    @Test
+    @WithMockUser(roles = "")
+    void test_myDetails_logoutUser() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/api/users/details"))
+                .andExpect(status().isForbidden())
+                .andDo(print())
+                .andReturn();
+
+        String body = mvcResult.getResponse().getContentAsString();
+        assertThat(body).isEqualTo("Access is denied");
+    }
+
+    @DisplayName("delete(ROLE_ADMIN)_존재하는 유저의 id인 경우")
     @Test
     void test_delete_existentUserId() throws Exception {
         MvcResult mvcResult = mockMvc.perform(post("/api/users/" + existentUser.getId() + "/delete"))
@@ -195,9 +235,10 @@ public class UserControllerTest {
         assertThat(body).isEqualTo("계정이 삭제되었습니다.");
     }
 
-    @DisplayName("delete_존재하지 않는 유저의 id인 경우")
+    @DisplayName("delete(ROLE_ADMIN)_존재하지 않는 유저의 id인 경우")
     @Test
     void test_delete_nonExistentUserId() throws Exception {
+
         doThrow(new NoUserFoundException())
                 .when(userManagementService)
                 .deleteById(999L);
@@ -211,7 +252,32 @@ public class UserControllerTest {
         assertThat(body).isEqualTo("해당 유저가 존재하지 않습니다.");
     }
 
-    @DisplayName("update_존재하는 유저인 경우")
+    @DisplayName("delete(ROLE_USER)_로그인 되어있을 경우")
+    @Test
+    void test_delete_loginUser() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(post("/api/users/delete"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        String body = mvcResult.getResponse().getContentAsString();
+        assertThat(body).isEqualTo("계정이 삭제되었습니다.");
+    }
+
+    @DisplayName("delete(ROLE_USER)_로그아웃 되어있을 경우 throw AccessDeniedException")
+    @Test
+    @WithMockUser(roles = "")
+    void test_delete_logoutUser() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(post("/api/users/delete"))
+                .andExpect(status().isForbidden())
+                .andDo(print())
+                .andReturn();
+
+        String body = mvcResult.getResponse().getContentAsString();
+        assertThat(body).isEqualTo("Access is denied");
+    }
+
+    @DisplayName("update(ROLE_USER)_존재하는 유저인 경우")
     @Test
     void test_update_existentUser() throws Exception {
         UserUpdateDto existentUserUpdate = new UserUpdateDto(this.existentUser.getId(), this.existentUser.getEmail(), "changePwd", "changePwd", "newNick");
@@ -224,10 +290,10 @@ public class UserControllerTest {
                 .andReturn();
 
         String body = mvcResult.getResponse().getContentAsString();
-        assertThat(body).isEqualTo("수정 성공");
+        assertThat(body).isEqualTo("정보를 변경했습니다. 다시 로그인하세요.");
     }
 
-    @DisplayName("update_존재하지 않는 유저인 경우 throw NoUserFoundException")
+    @DisplayName("update(ROLE_USER)_존재하지 않는 유저인 경우 throw NoUserFoundException")
     @Test
     void test_update_nonExistentUser() throws Exception {
         UserUpdateDto nonExistentUserUpdate = new UserUpdateDto(999L, "email@gmail.com", "changePwd", "changePwd", "newNick");
@@ -247,7 +313,7 @@ public class UserControllerTest {
         assertThat(body).isEqualTo("해당 유저가 존재하지 않습니다.");
     }
 
-    @DisplayName("update_비밀번호가 불일치인 경우 throw UnmatchedPasswordCheckException")
+    @DisplayName("update(ROLE_USER)_비밀번호가 불일치인 경우 throw UnmatchedPasswordCheckException")
     @Test
     void test_update_unmatchedPasswordUser() throws Exception {
         UserUpdateDto unmatchedPwdUserUpdate = new UserUpdateDto(existentUser.getId(), existentUser.getEmail(), "changePwd", "wrongPwd", "newNick");
@@ -267,7 +333,24 @@ public class UserControllerTest {
         assertThat(body).isEqualTo("비밀번호가 일치하지 않습니다.");
     }
 
-    @DisplayName("findAll_모든 유저 조회")
+    @DisplayName("update(ROLE_USER)_로그아웃 되어있을 경우 throw AccessDeniedException")
+    @Test
+    @WithMockUser(roles = "")
+    void test_update_logoutUser() throws Exception {
+        UserUpdateDto unmatchedPwdUserUpdate = new UserUpdateDto(existentUser.getId(), existentUser.getEmail(), "changePwd", "wrongPwd", "newNick");
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/users/update")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(unmatchedPwdUserUpdate)))
+                .andExpect(status().isForbidden())
+                .andDo(print())
+                .andReturn();
+
+        String body = mvcResult.getResponse().getContentAsString();
+        assertThat(body).isEqualTo("Access is denied");
+    }
+
+    @DisplayName("findAll(ROLE_ADMIN)_모든 유저 조회")
     @Test
     void test_findAll() throws Exception {
         List<UserResponseDto> users = new ArrayList<>();
@@ -291,5 +374,41 @@ public class UserControllerTest {
         assertThat(convertedUsers.size()).isEqualTo(users.size());
         assertThat(convertedUsers.get(0).getEmail()).isEqualTo(userResponseDto1.getEmail());
         assertThat(convertedUsers.get(1).getEmail()).isEqualTo(userResponseDto2.getEmail());
+    }
+
+    @DisplayName("login_정상 유저일 경우")
+    @Test
+    void test_login_normalUser() throws Exception {
+        UserLoginDto normalUserLogin = new UserLoginDto(existentUser.getEmail(), existentUser.getPassword());
+
+        doReturn("Afsd")
+                .when(authenticationManagerBuilder)
+                .getObject()
+                .authenticate(ArgumentMatchers.any(UsernamePasswordAuthenticationToken.class));
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(normalUserLogin)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+    }
+
+    @DisplayName("login_미등록 유저일 경우")
+    @Test
+    void test_login_unRegisteredUser() throws Exception {
+        UserLoginDto nonExistsUserLogin = new UserLoginDto(normalUserSignup.getEmail(), normalUserSignup.getPassword());
+
+        doThrow(BadCredentialsException.class)
+                .when(authenticationManagerBuilder)
+                .getObject()
+                .authenticate(ArgumentMatchers.any(UsernamePasswordAuthenticationToken.class));
+
+        MvcResult mvcResult = mockMvc.perform(post("/api/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(nonExistsUserLogin)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
     }
 }
--- src/test/java/com/skhu/luxuryshop/user/service/UserManagementServiceTest.java
+++ src/test/java/com/skhu/luxuryshop/user/service/UserManagementServiceTest.java
@@ -3,6 +3,9 @@ package com.skhu.luxuryshop.user.service;
 import com.skhu.luxuryshop.user.dto.UserResponseDto;
 import com.skhu.luxuryshop.user.dto.UserSignupDto;
 import com.skhu.luxuryshop.user.dto.UserUpdateDto;
+import com.skhu.luxuryshop.user.entity.Authority;
+import com.skhu.luxuryshop.user.entity.UserEntity;
+import com.skhu.luxuryshop.user.exception.DuplicatedEmailException;
 import com.skhu.luxuryshop.user.exception.NoUserFoundException;
 import com.skhu.luxuryshop.user.exception.UnmatchedPasswordCheckException;
 import com.skhu.luxuryshop.user.repository.UserRepository;
@@ -11,8 +14,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -26,17 +36,28 @@ public class UserManagementServiceTest {
     private UserManagementService userManagementService;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private AuthenticationManagerBuilder authenticationManagerBuilder;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     private UserSignupDto existentUser1;
-    private UserSignupDto existentUser2;
+    private Authority authority = Authority.builder()
+            .authorityName("ROLE_USER")
+            .build();
     private Long existentUserId1;
 
     @BeforeEach
     void setUp() {
         existentUser1 = new UserSignupDto("test1@gmail.com", "password", "password", "홍길동");
-        existentUser2 = new UserSignupDto("test2@gmail.com", "password", "password", "홍길서");
-        existentUserId1 = userRepository.save(existentUser1.toUserEntity()).getId();
-        userRepository.save(existentUser2.toUserEntity());
+
+        UserEntity existentUserEntity1 = UserEntity.builder()
+                .email(existentUser1.getEmail())
+                .password(passwordEncoder.encode(existentUser1.getPassword()))
+                .nickname(existentUser1.getNickname())
+                .authorities(Collections.singleton(authority))
+                .build();
+        existentUserId1 = userRepository.save(existentUserEntity1).getId();
     }
 
     @DisplayName("findById_존재하는 유저의 id인 경우")
@@ -53,6 +74,30 @@ public class UserManagementServiceTest {
     void test_findById_nonExistentUserId() {
         assertThatThrownBy(() -> {
             userManagementService.findById(999L);
+        }).isInstanceOf(NoUserFoundException.class)
+                .hasMessageContaining("해당 유저가 존재하지 않습니다.");
+    }
+
+    @DisplayName("findByLoginUser_로그인된 유저인 경우")
+    @Test
+    void test_findByLoginUser_loginUser() {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(existentUser1.getEmail(), existentUser1.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserResponseDto userResponseDto = userManagementService.findByLoginUser();
+
+        assertThat(userResponseDto.getEmail()).isEqualTo(existentUser1.getEmail());
+        assertThat(userResponseDto.getNickname()).isEqualTo(existentUser1.getNickname());
+    }
+
+    @DisplayName("findByLoginUser_로그아웃된 경우 throw NoUserFoundException")
+    @Test
+    void test_findByLoginUser_logoutUser() {
+        assertThatThrownBy(() -> {
+            userManagementService.findByLoginUser();
         }).isInstanceOf(NoUserFoundException.class)
                 .hasMessageContaining("해당 유저가 존재하지 않습니다.");
     }
@@ -74,9 +119,38 @@ public class UserManagementServiceTest {
                 .hasMessageContaining("해당 유저가 존재하지 않습니다.");
     }
 
-    @DisplayName("update_존재하는 유저인 경우")
+    @DisplayName("deleteByLoginUser_로그인된 유저인 경우")
     @Test
-    void test_update_existentUser() {
+    void test_deleteByLoginUser_loginUser() {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(existentUser1.getEmail(), existentUser1.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        userManagementService.deleteByLoginUser();
+
+        assertThat(false).isEqualTo(userRepository.existsById(existentUserId1));
+    }
+
+    @DisplayName("deleteByLoginUser_로그아웃된 경우 throw NoUserFoundException")
+    @Test
+    void test_deleteByLoginUser_logoutUser() {
+        assertThatThrownBy(() -> {
+            userManagementService.deleteByLoginUser();
+        }).isInstanceOf(NoUserFoundException.class)
+                .hasMessageContaining("해당 유저가 존재하지 않습니다.");
+    }
+
+    @DisplayName("update_로그인된 유저인 경우")
+    @Test
+    void test_update_loginUser() {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(existentUser1.getEmail(), existentUser1.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         UserUpdateDto existentUserUpdate = new UserUpdateDto(existentUserId1, "test1@gmail.com", "changedPwd", "changedPwd", "김북남");
 
         userManagementService.update(existentUserUpdate);
@@ -86,10 +160,10 @@ public class UserManagementServiceTest {
         assertThat(userResponseDto.getNickname()).isEqualTo(existentUserUpdate.getNickname());
     }
 
-    @DisplayName("update_존재하지 않는 유저인 경우 throw NoUserFoundException")
+    @DisplayName("update_로그아웃된 경우 throw NoUserFoundException")
     @Test
     void test_update_nonExistentUser() {
-        UserUpdateDto nonExistentUserUpdate = new UserUpdateDto(999L, "test1@gmail.com", "changedPwd", "changedPwd", "김북남");
+        UserUpdateDto nonExistentUserUpdate = new UserUpdateDto(existentUserId1, "test1@gmail.com", "changedPwd", "changedPwd", "김북남");
 
         assertThatThrownBy(() -> {
             userManagementService.update(nonExistentUserUpdate);
@@ -100,6 +174,12 @@ public class UserManagementServiceTest {
     @DisplayName("update_비밀번호 불일치 유저인 경우 throw UnmatchedPasswordCheckException")
     @Test
     void test_update_unmatchedPwdUser() {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(existentUser1.getEmail(), existentUser1.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         UserUpdateDto unmatchedPwdUserUpdate = new UserUpdateDto(existentUserId1, "test1@gmail.com", "changedPwd", "wrongPwd", "김북남");
 
         assertThatThrownBy(() -> {
@@ -108,14 +188,31 @@ public class UserManagementServiceTest {
                 .hasMessageContaining("비밀번호가 일치하지 않습니다.");
     }
 
+    @DisplayName("update_이메일 중복 유저인 경우 throw DuplicatedEmailException")
+    @Test
+    void test_update_duplicatedEmailUser() {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(existentUser1.getEmail(), existentUser1.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserUpdateDto duplicatedEmailUserUpdate = new UserUpdateDto(existentUserId1, existentUser1.getEmail(), "changedPwd", "changedPwd", "김북남");
+
+        assertThatThrownBy(() -> {
+            userManagementService.update(duplicatedEmailUserUpdate);
+        }).isInstanceOf(DuplicatedEmailException.class)
+                .hasMessageContaining("중복된 이메일입니다.");
+    }
+
     @DisplayName("findAll_모든 유저 탐색")
     @Test
     void test_findAll() {
         List<UserResponseDto> users = userManagementService.findAll();
         users.stream().sorted(Comparator.comparing((UserResponseDto userResponseDto) -> userResponseDto.getId()));
 
-        assertThat(users.size()).isEqualTo(2);
-        assertThat(users.get(0).getEmail()).isEqualTo(existentUser1.getEmail());
-        assertThat(users.get(0).getNickname()).isEqualTo(existentUser1.getNickname());
+        assertThat(users.size()).isEqualTo(3);
+        assertThat(users.get(2).getEmail()).isEqualTo(existentUser1.getEmail());
+        assertThat(users.get(2).getNickname()).isEqualTo(existentUser1.getNickname());
     }
 }
