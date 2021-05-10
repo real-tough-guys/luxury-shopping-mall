e0f18cdaac4b99a598908860e92535d5cc439628
jhj960918 <jhj960918@gmail.com>
2021-05-10 23:26:41 +0900

    requestDto 생성 기존 ProductEntity -> productId 수정

---
+++ src/main/java/com/skhu/luxuryshop/cart/dto/CartRequestDto.java
@@ -0,0 +1,16 @@
+package com.skhu.luxuryshop.cart.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
+@Getter
+@AllArgsConstructor
+public class CartRequestDto {
+    @Positive(message = "없음")
+    private Long productId;
+    @NotBlank(message = "색상.")
+    private String color;
+}
