92f722d97a7e19cdcc5b3621fb99bac2c3c8a82c
jhj960918 <jhj960918@gmail.com>
2021-05-10 23:22:26 +0900

    vue filter mixin 적용(중복코드 제

--- src/frontend/src/views/Main.vue
+++ src/frontend/src/views/Main.vue
@@ -3,25 +3,43 @@
     <v-card class="mx-auto overflow-hidden" height="auto">
       <v-container fluid>
         <v-row>
-          <v-col v-for="(item, i) in listData" :key="`item-${i}`" cols="12" sm="4">
+          <v-col
+            v-for="(item, i) in listData"
+            :key="`item-${i}`"
+            cols="12"
+            sm="4"
+          >
             <div>
               <v-hover v-slot="{ hover }">
-
                 <v-card class="mx-auto" max-width="344">
                   <div @click="detailPush(item.id)">
-                    <v-img v-bind:src="item.productImageurl[0] |loadImgOrPlaceholder" :aspect-ratio="8/8"
-                           height="mx-auto">
+                    <v-img
+                      v-bind:src="
+                        item.productImageurl[0] | loadImgOrPlaceholder
+                      "
+                      :aspect-ratio="11 / 13"
+                      height="mx-auto"
+                    >
                       <v-expand-transition>
-                        <div v-if="hover"
-                             class="d-flex transition-fast-in-fast-out blue-grey darken-2 v-card--reveal display-3 white--text"
-                             style="height: 100%;">
-                        </div>
+                        <div
+                          v-if="hover"
+                          class="d-flex transition-fast-in-fast-out blue-grey darken-2 v-card--reveal display-3 white--text"
+                          style="height: 100%;"
+                        ></div>
                       </v-expand-transition>
                     </v-img>
                   </div>
                   <v-card-text class="pt-6" style="position: relative;">
-                    <v-btn absolute color="black" class="white--text" fab large right top>
-                      <v-icon>mdi-cart</v-icon>
+                    <v-btn
+                      absolute
+                      color="black"
+                      class="white--text"
+                      fab
+                      large
+                      right
+                      top
+                    >
+                      <v-icon>mdi-heart</v-icon>
                     </v-btn>
 
                     <div class="font-weight-bold black--text title mb-2">
@@ -31,7 +49,7 @@
                       {{ item.productContent }}
                     </div>
                     <div class="font-weight-medium title mb-2">
-                      {{ item.productPrice | money }} won
+                      {{ item.productPrice | moneyFilter }} won
                     </div>
                   </v-card-text>
                 </v-card>
@@ -40,29 +58,28 @@
           </v-col>
         </v-row>
       </v-container>
-      <infinite-loading @infinite="infiniteHandler" spinner="waveDots" forceUseInfiniteWrapper></infinite-loading>
+      <infinite-loading
+        @infinite="infiniteHandler"
+        spinner="waveDots"
+        forceUseInfiniteWrapper
+      ></infinite-loading>
     </v-card>
+
   </div>
 </template>
 
 <script>
 import InfiniteLoading from 'vue-infinite-loading';
 import axios from 'axios';
+import myMixin from "@/filter";
 
 export default {
+  mixins: [myMixin],
   data() {
     return {
       limit: 1,
       listData: [],
     };
-  },
-  filters: {
-    loadImgOrPlaceholder: function (path) {
-      return require("@/assets/images/" + path)
-    },
-    money: function (value) {
-      return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    },
   },
   components: {
     InfiniteLoading,
