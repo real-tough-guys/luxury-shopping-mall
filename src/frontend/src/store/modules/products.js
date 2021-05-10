9fa60e740fa3bac0c37aedb6146176a6f628d7ca
jhj960918 <jhj960918@gmail.com>
2021-05-10 23:20:11 +0900

    vuex 임시

--- src/frontend/src/store/modules/products.js
+++ src/frontend/src/store/modules/products.js
@@ -1,30 +1,48 @@
 import axios from "axios";
+
 const state = {
-  productList: null
+    productList: null,
+    productDetail: null
 };
 const getters = {};
 const actions = {
-  getProductsList({ commit }) {
-    return axios
-      .get("/api/products")
-      .then(data => {
-        commit("setProductList", data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+    getProductsList({commit}) {
+        return axios
+            .get("/api/products")
+            .then(data => {
+                commit("setProductList", data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    },
+    getProduct({commit}, id) {
+        console.log("detail...." +id)
+        return axios
+            .get("/api/products/", {id:id})
+            .then(data => {
+                console.log(data)
+                commit("setProduct", data);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
 };
 
 const mutations = {
     setProductList(state, data) {
         state.productList = data;
     },
+    setProduct(state,data){
+        console.log(data)
+        state.productDetail=data;
+    }
 };
 export default {
-  namespaced: true,
-  getters,
-  state,
-  actions,
-  mutations
+    namespaced: true,
+    getters,
+    state,
+    actions,
+    mutations
 };
