2b11997ba0a1b923d7e25e8c01805877939632d0
jhj960918 <jhj960918@gmail.com>
2021-05-17 14:51:59 +0900

    search repository 상품이름,상품 카테고리 필터링

--- src/main/java/com/skhu/luxuryshop/product/repository/ProductRepository.java
+++ src/main/java/com/skhu/luxuryshop/product/repository/ProductRepository.java
@@ -4,7 +4,11 @@ import com.skhu.luxuryshop.product.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     boolean existsById(Long id);
+    List<ProductEntity> findByProductNameContaining(String keyword);
+    List<ProductEntity> findByProductCategoryContaining(String keword);
 }
